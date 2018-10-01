2108f6d4b4bdf86b8335ef66891c109abf1d096d
1m188 <228117330@qq.com>
2018-10-01 15:29:40 +0800

    感觉之前做的有点复杂，，，写不出来，，，还是从简单的开始好了，删掉了所有场景，全部做到主界面里，后面再进行范围性重构和完善。 添加了相关的一些基础初始化。当然，代码写的很没有灵活性。

--- PlayBricks/maingui.cpp
+++ PlayBricks/maingui.cpp
@@ -1,7 +1,7 @@
 #include "maingui.h"
 #include "QApplication"
 #include "QDeskTopWidget"
-#include "gamescene.h"
+#include "QKeyEvent"
 
 MainGui::MainGui(QWidget *parent)
 	: QWidget(parent)
@@ -11,7 +11,7 @@ MainGui::MainGui(QWidget *parent)
 	setAttribute(Qt::WA_QuitOnClose, true);
 
 	//标题和大小
-	setWindowTitle(tr("QtGame"));
+	setWindowTitle(tr(u8"打砖块"));
 	setFixedSize(1000, 800);
 
 	//移动到屏幕中央
@@ -19,12 +19,51 @@ MainGui::MainGui(QWidget *parent)
 	rect.moveCenter(QApplication::desktop()->availableGeometry().center());
 	move(rect.topLeft());
 
-	//游戏场景
-	GameScene *gameScene = new GameScene(this);
-	gameScene->setFixedSize(size());
+	//游戏相关初始化
+	isStart = false; //游戏还未开始
+
+	//图片初始化
+	ballPixmap.load(":/PlayBricks/Resources/ball.png");
+	paddlePixmap.load(":/PlayBricks/Resources/paddle.png");
+	block_bluePixmap.load(":/PlayBricks/Resources/block_blue.png");
+	block_greenPixmap.load(":/PlayBricks/Resources/block_green.png");
+	block_redPixmap.load(":/PlayBricks/Resources/block_red.png");
+
+	//挡板初始化
+	paddleLabel = new QLabel(this);
+	paddleLabel->setPixmap(paddlePixmap);
+	paddleLabel->setFixedSize(paddlePixmap.size());
+	paddleLabel->move(width() / 2 - paddleLabel->width() / 2, height() - paddleLabel->height() - 10);
+
+	//球初始化
+	ballLabel = new QLabel(this);
+	ballLabel->setPixmap(ballPixmap);
+	ballLabel->setFixedSize(ballPixmap.size());
+	ballLabel->move(paddleLabel->x() + paddleLabel->width() / 2 - ballLabel->width() / 2, paddleLabel->y() - ballLabel->height() - 10);
+
+	//砖块初始化
+	for (int i = 0; i < 10; i++)
+	{
+		blockLabelVector.append(QVector<QLabel *>{});
+		for (int j = 0; j < 16; j++)
+		{
+			QLabel *blockLabel = new QLabel(this);
+			blockLabel->setPixmap(block_bluePixmap);
+			blockLabel->setFixedSize(block_bluePixmap.size());
+			blockLabel->move(j*block_bluePixmap.width(), i*block_bluePixmap.height());
+			blockLabelVector[blockLabelVector.size() - 1].append(blockLabel);
+		}
+	}
 }
 
 MainGui::~MainGui()
 {
 
+}
+
+void MainGui::keyPressEvent(QKeyEvent * event)
+{
+#ifdef _DEBUG
+	qDebug() << event->key();
+#endif // _DEBUG
 }
--- PlayBricks/maingui.h
+++ PlayBricks/maingui.h
@@ -2,6 +2,13 @@
 #define MAINGUI_H
 
 #include <QWidget>
+#include "QPixmap"
+#include "QLabel"
+
+#ifdef _DEBUG
+#include "QDebug"
+#endif // _DEBUG
+
 
 //游戏主界面
 class MainGui : public QWidget
@@ -13,7 +20,22 @@ public:
 	~MainGui();
 
 private:
+	//一系列资源图片
+	QPixmap ballPixmap;
+	QPixmap paddlePixmap;
+	QPixmap block_bluePixmap;
+	QPixmap block_greenPixmap;
+	QPixmap block_redPixmap;
 
+	//一些游戏元素的标签
+	QLabel *ballLabel;
+	QLabel *paddleLabel;
+	QVector<QVector<QLabel *>> blockLabelVector; //砖块二维数组
+
+	bool isStart; //游戏是否开始
+
+protected:
+	void keyPressEvent(QKeyEvent *event) override; //重写按键事件
 };
 
 #endif // MAINGUI_H

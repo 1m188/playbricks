e8dedb12bb31b81b92fc062ed380d92c7c2dc344
1m188 <228117330@qq.com>
2018-11-12 12:17:13 +0800

    添加了场景基类

---
+++ PlayBricks/Scene.h
@@ -0,0 +1,18 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+#include "Window.h"
+
+//场景类
+class Scene : public QWidget
+{
+	Q_OBJECT
+
+public:
+	Scene(Window *parent);
+	virtual ~Scene();
+
+	virtual void init(); //初始化场景
+};
+
+#endif // SCENE_H

dafd3647cea2f7ab90958ca8cd549994fced9062
1m188 <228117330@qq.com>
2018-11-12 12:59:10 +0800

    加入了开始场景，有两个选项，一个是开始新游戏，一个是退出

---
+++ PlayBricks/StartScene.cpp
@@ -0,0 +1,52 @@
+#include "StartScene.h"
+#include "Director.h"
+#include "GameScene.h"
+#include "QLabel"
+#include "QPushButton"
+#include "QGridLayout"
+#include "QApplication"
+
+StartScene::StartScene(Window *parent)
+	: Scene(parent)
+{
+
+}
+
+StartScene::~StartScene()
+{
+
+}
+
+void StartScene::init()
+{
+	//¿Ø¼þ
+	QLabel *infoLabel = new QLabel(this);
+	infoLabel->setAlignment(Qt::AlignCenter);
+	infoLabel->setFont(QFont(u8"Î¢ÈíÑÅºÚ", 40));
+	infoLabel->setText(tr(u8"´ò  ×©  ¿é"));
+
+	QPushButton *newGameButton = new QPushButton(this);
+	newGameButton->setFont(QFont(u8"Î¢ÈíÑÅºÚ", 15));
+	newGameButton->setText(tr(u8"¿ªÊ¼ÐÂÓÎÏ·"));
+	connect(newGameButton, &QPushButton::clicked, this, &StartScene::newGameButtonClicked);
+
+	QPushButton *exitButton = new QPushButton(this);
+	exitButton->setFont(QFont(u8"Î¢ÈíÑÅºÚ", 15));
+	exitButton->setText(tr(u8"ÍË³ö"));
+	connect(exitButton, &QPushButton::clicked, QApplication::quit);
+
+	//²¼¾Ö
+	QGridLayout *layout = new QGridLayout(this);
+	layout->addWidget(infoLabel, 0, 0, 10, 15);
+	layout->addWidget(newGameButton, 10, 5, 3, 5);
+	layout->addWidget(exitButton, 13, 5, 3, 5);
+}
+
+void StartScene::newGameButtonClicked()
+{
+	GameScene *gameScene = new GameScene(Director::getInstance()->getWindow());
+	Director::getInstance()->setNowScene(gameScene);
+	gameScene->init();
+	gameScene->show();
+	deleteLater();
+}
---
+++ PlayBricks/StartScene.h
@@ -0,0 +1,21 @@
+#ifndef STARTSCENE_H
+#define STARTSCENE_H
+
+#include "Scene.h"
+
+//开始场景
+class StartScene : public Scene
+{
+	Q_OBJECT
+
+public:
+	StartScene(Window *parent);
+	~StartScene();
+
+	void init() override;
+
+	private slots:
+	void newGameButtonClicked(); //单击开始新游戏按钮
+};
+
+#endif // STARTSCENE_H

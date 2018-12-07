b2a79d586055b858b6b7be6fd54195e9bef16242
1m188 <228117330@qq.com>
2018-12-07 14:13:58 +0800

    唉，大错特错，一不小心，现在又要从头写起。

---
+++ PlayBricks/Brick.cpp
@@ -0,0 +1,16 @@
+#include "Brick.h"
+
+Brick::Brick() :Sprite()
+{
+
+}
+
+Brick::Brick(int x, int y, int width, int height, QPixmap image, bool isCollided) : Sprite(x, y, width, height, image), isCollided(isCollided)
+{
+
+}
+
+Brick::~Brick()
+{
+
+}
---
+++ PlayBricks/Brick.h
@@ -0,0 +1,23 @@
+#ifndef BRICK_H
+#define BRICK_H
+
+#include "Sprite.h"
+
+//单个砖块类
+class Brick :Sprite
+{
+private:
+	//是否被撞过
+	bool isCollided;
+
+public:
+	Brick();
+	Brick(int x, int y, int width, int height, QPixmap image, bool isCollided);
+	~Brick();
+
+	//获取/设置是否被撞过
+	const bool getIsCollided() const { return isCollided; }
+	void setIsCollided(bool isCollided) { this->isCollided = isCollided; }
+};
+
+#endif // BRICK_H

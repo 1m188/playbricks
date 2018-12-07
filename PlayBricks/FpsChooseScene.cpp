a04e2ac03c856995a872e614a674a335a7410389
1m188 <228117330@qq.com>
2018-12-08 00:32:07 +0800

    代码规范

--- PlayBricks/FpsChooseScene.cpp
+++ PlayBricks/FpsChooseScene.cpp
@@ -28,26 +28,26 @@ void FpsChooseScene::init()
 	fpsLabel->setFont(QFont(u8"Î¢ÈíÑÅºÚ", 20));
 	fpsLabel->setText(tr(u8"FPS"));
 
-	QLabel *sliderValueLabel = new QLabel(this);
-	sliderValueLabel->setAlignment(Qt::AlignCenter);
-	sliderValueLabel->setFont(QFont(u8"Î¢ÈíÑÅºÚ", 20));
-	sliderValueLabel->setText(QString::number(30));
+	QLabel *fpsValueLabel = new QLabel(this);
+	fpsValueLabel->setAlignment(Qt::AlignCenter);
+	fpsValueLabel->setFont(QFont(u8"Î¢ÈíÑÅºÚ", 20));
+	fpsValueLabel->setText(QString::number(30));
 
-	QSlider *slider = new QSlider(this);
-	slider->setOrientation(Qt::Horizontal);
-	slider->setMinimum(30);
-	slider->setMaximum(60);
-	slider->setSingleStep(1);
+	QSlider *fpsSlider = new QSlider(this);
+	fpsSlider->setOrientation(Qt::Horizontal);
+	fpsSlider->setMinimum(30);
+	fpsSlider->setMaximum(60);
+	fpsSlider->setSingleStep(1);
 
-	QPushButton *returnToStartScene = new QPushButton(this);
-	returnToStartScene->setFont(QFont(u8"Î¢ÈíÑÅºÚ", 15));
-	returnToStartScene->setText(tr(u8"·µ»Ø¿ªÊ¼½çÃæ"));
+	QPushButton *returnToStartSceneButton = new QPushButton(this);
+	returnToStartSceneButton->setFont(QFont(u8"Î¢ÈíÑÅºÚ", 15));
+	returnToStartSceneButton->setText(tr(u8"·µ»Ø¿ªÊ¼½çÃæ"));
 
 	//²¼¾Ö
 	QGridLayout *layout = new QGridLayout(this);
 	layout->addWidget(infoLabel, 0, 0, 10, 15);
 	layout->addWidget(fpsLabel, 10, 0, 4, 5);
-	layout->addWidget(slider, 10, 5, 4, 5);
-	layout->addWidget(sliderValueLabel, 10, 10, 4, 5);
-	layout->addWidget(returnToStartScene, 14, 5, 4, 5);
+	layout->addWidget(fpsSlider, 10, 5, 4, 5);
+	layout->addWidget(fpsValueLabel, 10, 10, 4, 5);
+	layout->addWidget(returnToStartSceneButton, 14, 5, 4, 5);
 }
